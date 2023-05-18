3c012637fcf999c1765028574d3b5cdc3d3150fd
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-05-18 17:42:14 +0000

    cambios en ficheros home y trafficlight

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -10,17 +10,17 @@ const TrafficLight = ({selectedLight, selectedColor}) => {
                 <div className="row">
                     <div className="col-1 bg-black mx-auto rounded-4 py-3" id="box">
                         <div className="row">
-                            <button onClick={()=>{selectedLight("circleRed")}} className={`circleRed col-10 rounded-circle bg-danger mx-auto my-2  ${
-                  selectedColor === "circleRed" ? "border-5 border-light" : "border-0"}
+                            <button onClick={()=>{selectedLight("circleRed")}} className={`circleRed col-10 rounded-circle bg-danger mx-auto my-2 border-0 ${
+                  selectedColor === "circleRed" ? " brillante1" : "border-0"}
                   `}></button>
                         </div>
                         <div className="row">
-                            <button onClick={()=>{selectedLight("circleYellow")}} className={`circleYellow col-10 rounded-circle bg-warning mx-auto my-2  ${
-                  selectedColor === "circleYellow" ? "border-5 border-light" : "border-0"}`}></button>
+                            <button onClick={()=>{selectedLight("circleYellow")}} className={`circleYellow col-10 rounded-circle bg-warning mx-auto my-2 border-0 ${
+                  selectedColor === "circleYellow" ? "brillante2" : "border-0"}`}></button>
                         </div>
                         <div className="row">
-                            <button onClick={()=>{selectedLight("circleGreen")}} className={`circleGreen col-10 rounded-circle bg-success mx-auto my-2  ${
-                  selectedColor === "circleGreen" ? "border-5 border-light" : "border-0"}`}></button>
+                            <button onClick={()=>{selectedLight("circleGreen")}} className={`circleGreen col-10 rounded-circle bg-success mx-auto my-2 border-0 ${
+                  selectedColor === "circleGreen" ? "brillante3" : "border-0"}`}></button>
                         </div>
                     </div>
                 </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,11 +4,15 @@ import TrafficLight from "./TrafficLight.js";
 //create your first component
 const Home = () => {
 	const [light, setLight] = useState("")
-
+	
+	const stop = () => {
+		setLight("circleRed");
+	  };
 
 	return (
 		<div className="text-center">
 			<TrafficLight selectedLight={setLight} selectedColor={light} />
+			<button className="btn btn-danger mt-5" onClick={stop}>STOP</button>
 		</div>
 	);
 };
